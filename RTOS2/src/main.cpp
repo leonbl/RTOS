620a6ebb25a71d6a2342eca1c9df18fa246c0202
leonbl <leon.blazic@gmail.com>
2023-10-10 09:09:37 +0200

    tipke2

--- RTOS2/src/main.cpp
+++ RTOS2/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 int staro_stanje;
+int lucka = 10;
 void setup()
 {
   pinMode(A1, INPUT);
   pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
@@ -11,7 +15,13 @@ void loop()
   int stanje = digitalRead(A1);
   if (staro_stanje == 1 && stanje == 0)
   {
-    digitalWrite(10, !digitalRead(10));
+    lucka++;
+    if(lucka==10)digitalWrite(13, HIGH);
+    digitalWrite(lucka, LOW);
+    digitalWrite(lucka-1, HIGH);
+    if(lucka>=13) lucka=9;
+
   }
+  delay(100);
   staro_stanje = stanje;
 }
