df7196d8f215195b860a1bbe7e4309e503756c76
leonbl <leon.blazic@gmail.com>
2023-11-14 08:38:46 +0100

    ...

--- RTOS4/src/main.cpp
+++ RTOS4/src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
 
-uint32_t stevec = 0; 
+uint32_t stevec = 0;
 void nitLed1(void *args);
 void nitLed2(void *args);
 void nitLed3(void *args);
 void nitSerijski(void *args);
+
+TaskHandle_t led1_h;
+TaskHandle_t led2_h;
+TaskHandle_t led3_h;
+TaskHandle_t ser_h;
 
 void setup()
 {
@@ -13,46 +18,53 @@ void setup()
   pinMode(13, OUTPUT);
   pinMode(11, OUTPUT);
   Serial.begin(9600);
-  xTaskCreate(nitLed1,NULL,256,NULL,1,NULL);
-  xTaskCreate(nitLed2,NULL,256,NULL,1,NULL);
-  xTaskCreate(nitLed3,NULL,256,NULL,1,NULL);
-  xTaskCreate(nitSerijski,NULL,256,NULL,1,NULL);
+  xTaskCreate(nitLed1, NULL, 256, NULL, 1, &led1_h);
+  xTaskCreate(nitLed2, NULL, 256, NULL, 1, &led2_h);
+  xTaskCreate(nitLed3, NULL, 256, NULL, 1, &led3_h);
+  xTaskCreate(nitSerijski, NULL, 256, NULL, 1, &ser_h);
   vTaskStartScheduler();
 }
 
-void nitLed1(void *args){
-  while(1){
+void nitLed1(void *pvParameters)
+{
+  while (1)
+  {
     digitalWrite(11, HIGH);
-    vTaskDelay(600/portTICK_PERIOD_MS);
+    vTaskDelay(600 / portTICK_PERIOD_MS);
     digitalWrite(11, LOW);
-    vTaskDelay(600/portTICK_PERIOD_MS);
+    vTaskDelay(600 / portTICK_PERIOD_MS);
   }
 }
 
-void nitLed2(void *args){
-  while(1){
+void nitLed2(void *args)
+{
+  while (1)
+  {
     digitalWrite(12, HIGH);
-    vTaskDelay(500/portTICK_PERIOD_MS);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
     digitalWrite(12, LOW);
-    vTaskDelay(500/portTICK_PERIOD_MS);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
-void nitLed3(void *args){
-  while(1){
+void nitLed3(void *args)
+{
+  while (1)
+  {
     digitalWrite(13, HIGH);
-    vTaskDelay(400/portTICK_PERIOD_MS);
+    vTaskDelay(400 / portTICK_PERIOD_MS);
     digitalWrite(13, LOW);
-    vTaskDelay(400/portTICK_PERIOD_MS);
+    vTaskDelay(400 / portTICK_PERIOD_MS);
   }
 }
 
-void nitSerijski(void *args){
-  while(1){
-    Serial.println(stevec++);
-    vTaskDelay(500/portTICK_PERIOD_MS);
+void nitSerijski(void *args)
+{
+  while (1)
+  {
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    Serial.print(uxTaskGetStackHighWaterMark(led1_h));
   }
 }
 
-void loop(){}
-
+void loop() {}
